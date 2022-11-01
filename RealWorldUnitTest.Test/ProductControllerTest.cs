57680593b57f781a58fd655ae1c074a5ec2abba2
velibeyli <valibayli91@gmail.com>
2022-11-01 15:57:50 +0400

    tested second details method

--- RealWorldUnitTest.Test/ProductControllerTest.cs
+++ RealWorldUnitTest.Test/ProductControllerTest.cs
@@ -64,5 +64,20 @@ namespace RealWorldUnitTest.Test
 
             Assert.Equal("Index",redirect.ActionName);
         }
+
+        [Fact]
+        public async void Details_InvalidId_ReturnNotFound()
+        {
+            Product product = null;
+
+            _mockRepo.Setup(x => x.GetById(0)).ReturnsAsync(product);
+
+            var result = await _controller.Details(0);
+
+            var redirect = Assert.IsType<NotFoundResult>(result);
+
+            Assert.Equal<int>(404,redirect.StatusCode); 
+
+        }
     }
 }
