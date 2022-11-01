b382ee8c47e665c5a7cfc9f87b81ceab5e9a8ed3
velibeyli <valibayli91@gmail.com>
2022-11-01 16:58:30 +0400

    tested third details method

--- RealWorldUnitTest.Test/ProductControllerTest.cs
+++ RealWorldUnitTest.Test/ProductControllerTest.cs
@@ -79,5 +79,24 @@ namespace RealWorldUnitTest.Test
             Assert.Equal<int>(404,redirect.StatusCode); 
 
         }
+
+        [Theory]
+        [InlineData(4)]
+        public async void Details_ValidId_ReturnProduct(int productId)
+        {
+            Product product = products.First(x => x.Id == productId);
+
+            _mockRepo.Setup(x => x.GetById(productId)).ReturnsAsync(product);
+
+            var result = await _controller.Details(productId);
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            var resultProduct = Assert.IsAssignableFrom<Product>(viewResult.Model);
+
+            Assert.Equal(product.Id,resultProduct.Id);
+            Assert.Equal(product.Name, resultProduct.Name);
+
+        }
     }
 }
