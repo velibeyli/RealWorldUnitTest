e3d6fec372b62fccd8cc42dccd953f2c17c9c701
velibeyli <valibayli91@gmail.com>
2022-11-01 22:05:35 +0400

    tested never executed create method

--- RealWorldUnitTest.Test/ProductControllerTest.cs
+++ RealWorldUnitTest.Test/ProductControllerTest.cs
@@ -124,5 +124,17 @@ namespace RealWorldUnitTest.Test
 
             Assert.Equal(products.First().Id, newProduct.Id);
         }
+
+        [Fact]
+        public async void CreatePOST_InvalidModelState_NeverCreateExecute()
+        {
+            _controller.ModelState.AddModelError("Name","");
+
+            var result = await _controller.Create(products.First());
+
+            _mockRepo.Verify(repo => repo.Create(It.IsAny<Product>()),Times.Never);
+
+
+        }
     }
 }
